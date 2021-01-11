951d840211cbbfd4010e8aefa64664a40df036e0
marimunari <54755896+marimunari@users.noreply.github.com>
2021-01-10 23:42:53 -0300

    Operações Matemáticas

---
+++ aula_01/operacoes_matematicas.py
@@ -0,0 +1,26 @@
+n1 = 10
+n2 = 3
+
+## Soma
+res = n1 + n2
+print(res)
+
+## Subtração
+res = n1 - n2
+print(res)
+
+## Multiplicação
+res = n1 * n2
+print(res)
+
+## Divisão
+res = n1 / n2
+print(res)
+
+## Divisão (inteiro)
+res = n1 // n2
+print(res)
+
+## Módulo
+res = n1 % n2
+print(res)
