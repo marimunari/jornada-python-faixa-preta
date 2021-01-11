fc59e258b3d39f05416b74698733df71535dcd86
marimunari <54755896+marimunari@users.noreply.github.com>
2021-01-10 23:42:34 -0300

    Tipos de Dados

---
+++ aula_01/tipos_dados.py
@@ -0,0 +1,11 @@
+## Atribuindo valores as variáveis
+inteiro = 10
+decimal = 10.5
+texto = 'Mariana Munari'
+boolean = True
+
+## Verificando os tipos das variáveis
+print(type(inteiro))
+print(type(decimal))
+print(type(texto))
+print(type(boolean))
