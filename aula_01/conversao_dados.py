5960ce54c656ce5d8781ca4d5484c59a07822ec7
marimunari <54755896+marimunari@users.noreply.github.com>
2021-01-10 23:43:13 -0300

    Conversão de Dados

---
+++ aula_01/conversao_dados.py
@@ -0,0 +1,14 @@
+## Atribuindo valores as variáveis
+ano = 2021
+idade = '20'
+peso = 47
+
+## Convertendo os valores das variáveis
+idade = int(idade)
+ano = str(ano)
+peso = float(peso)
+
+## Verificando os tipos das variáveis
+print(type(ano))
+print(type(idade))
+print(type(peso))
