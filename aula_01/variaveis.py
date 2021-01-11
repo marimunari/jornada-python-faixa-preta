b43c8788a4a8ee8d85f942e155ae739bd80ca3db
marimunari <54755896+marimunari@users.noreply.github.com>
2021-01-10 23:41:45 -0300

    Declaração de Variáveis

---
+++ aula_01/variaveis.py
@@ -0,0 +1,10 @@
+## Declaração de variáveis
+idade = 20
+nomeCompleto = 'Mariana Munari'
+
+## Exibindo valores das variáveis
+print(idade)
+print(nomeCompleto)
+
+## Concatenação
+print(idade, nomeCompleto)
